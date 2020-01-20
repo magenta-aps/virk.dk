d0249f215dd72845dcefa8ebf33ec76d6df46023
heiniovason <heini.ovason@gmail.com>
2020-01-20 13:52:17 +0100

    file_path added as env var. test read file OK

--- app.py
+++ app.py
@@ -4,6 +4,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
+# Contributor(s): Heini L. Ovason
+#
 
 import json
 import requests
@@ -15,4 +17,21 @@ credentials = {
     'password': settings.VIRK_DK_PASSWORD,
 }
 
-print(json.dumps(credentials))
+# input_file must be utf-8 encoded.
+input_file = open(settings.INPUT_FILE, "r")
+
+for line in input_file:
+
+    addr_line_array = line.split(";")
+
+    navn = addr_line_array[0]
+    vejnavn = addr_line_array[1].rsplit(" ", 1)[0]
+    husnr = addr_line_array[1].rsplit(" ", 1)[1]
+    postnr = addr_line_array[2].split(" ")[0]
+
+    print("{0} - {1} - {2} - {3}".format(navn, vejnavn, husnr, postnr))
+
+    # TODO
+    # Create function to build JSON query
+
+input_file.close()
