030b3232d8e1ee5a942f94033a04b69cfc5bdf91
heiniovason <heini.ovason@gmail.com>
2020-01-21 11:32:18 +0100

    P.O.C. v1 done

--- app.py
+++ app.py
@@ -12,24 +12,31 @@ import requests
 import settings
 from jinja2 import Template
 
+success_file = open(settings.SUCCESS_FILE, "a")
+success_file.write(f"CVRNR;NAVN;VEJNAVN;HUSNR;POSTNR;INPUT/SØGEPARAMETRE;\n")
+
+failed_file = open(settings.FAILED_FILE, "a")
+
 # input_file must be utf-8 encoded.
-input_file = open(settings.INPUT_FILE, "r")
+input_file = open(settings.INPUT_FILE, "r", encoding="utf-8")
 
 for line in input_file:
 
     addr_line_array = line.split(";")
 
-    navn = addr_line_array[0]
+    navn = addr_line_array[0].replace("/", "\\\\/") 
     vejnavn = addr_line_array[1].rsplit(" ", 1)[0]
     husnr = addr_line_array[1].rsplit(" ", 1)[1]
     postnr = addr_line_array[2].split(" ")[0]
+
+    search_params = f"{navn}+{vejnavn}+{husnr}+{postnr}"
 
     with open(settings.QUERY_TEMPLATE, "r") as filestream:
         template_string = filestream.read()
 
     template_object = Template(template_string)
 
-    populate_template = template_object.render(
+    populated_template = template_object.render(
         navn=navn,
         vejnavn=vejnavn,
         husnr=husnr,
@@ -39,20 +46,40 @@ for line in input_file:
     url = settings.VIRK_DK_ENDPOINT
     usr = settings.VIRK_DK_USERNAME
     pwd = settings.VIRK_DK_PASSWORD
-    headers = {'Content-type': 'application/json'}
+    headers = {"Content-type": "application/json; charset=UTF-8"}
+    payload = json.loads(populated_template)
     
-    r = requests.post(
+    resp = requests.post(
         url, 
         auth=(usr, pwd),
-        data=populate_template,
+        json=payload,
         headers=headers
         )
 
-    response_body = print(r.text)
+    if resp.status_code is 200:
+        try:
+            resp_len = len(json.loads(resp.text).get("hits").get("hits"))
+            if resp_len == 1:
+                virksomhed = json.loads(resp.text).get("hits").get("hits")[0].get("_source").get("Vrvirksomhed")
+                cvrNo = virksomhed.get("cvrNummer", "NODATA")
+                r_navn = virksomhed.get("virksomhedMetadata").get("nyesteNavn").get("navn", "NODATA")
+                r_adresse = virksomhed.get("virksomhedMetadata").get("nyesteBeliggenhedsadresse")
+                r_vejnavn = r_adresse.get("vejnavn", "NODATA")
+                r_husnr = r_adresse.get("husnummerFra", "NODATA")
+                r_postnr = r_adresse.get("postnummer", "NODATA")           
+                success_file.write(f"{cvrNo};{r_navn};{r_vejnavn};{r_husnr};{r_postnr};{search_params};\n")
+            else:
+                failed_file.write(f"Firmanavn findes ikke på virk.dk -->{line}\n")
+        except AttributeError as ae:
+                failed_file.write(f"AttributeError -->{ae}\n")
+                print(ae)
 
-    break 
-    # TODO
-    # Create function to build JSON query
+    else:
+        failed_file.write(f"HTTP Error -->{resp.status_code}\nHTTP Response Body --> {resp.text}\n\n")
+        print(populated_template)
 
 input_file.close()
+success_file.close()
+failed_file.close()
+
 
