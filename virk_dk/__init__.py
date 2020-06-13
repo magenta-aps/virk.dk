268342f448fedddc94cd1adb87e031cfe1489b38
heiniovason <heini.ovason@gmail.com>
2020-06-13 11:24:13 +0200

    get_org_info() implemented. Conditional statements added to jinja template.

--- virk_dk/__init__.py
+++ virk_dk/__init__.py
@@ -1,1 +1,2 @@
 from virk_dk.org_lookup import get_cvr_no
+from virk_dk.org_lookup import get_org_info
--- virk_dk/org_lookup.py
+++ virk_dk/org_lookup.py
@@ -13,6 +13,115 @@ import requests
 from jinja2 import Template
 
 
+def get_org_info(params_dict):
+    """Explanation pending
+    """
+    virk_usr = params_dict.get("virk_usr", None)
+    virk_pwd = params_dict.get("virk_pwd", None)
+    virk_url = params_dict.get("virk_url", None)
+
+    # No login, no search.
+    if virk_usr and virk_pwd and virk_url:
+
+        cvr = params_dict.get("cvr", None)
+
+        # If logged in then these params are the mininimum requirements.
+        if cvr:
+
+            here = os.path.dirname(os.path.abspath(__file__))
+            template = os.path.join(here, 'query.j2')
+            with open(template, "r") as filestream:
+                template_string = filestream.read()
+
+            template_object = Template(template_string)
+
+            populated_template = template_object.render(
+                cvr=cvr
+            )
+
+            url = virk_url
+            usr = virk_usr
+            pwd = virk_pwd
+            headers = {"Content-type": "application/json; charset=UTF-8"}
+            
+            # json.decoder.JSONDecodeError does NOT LIKE the linebreaks in 
+            # the ElasticSearch query in the template. 
+            # Therefore we remove them before deserializing. 
+            payload = json.loads(populated_template.replace('\n',''))
+            
+            resp = requests.post(
+                url,
+                auth=(usr, pwd),
+                json=payload,
+                headers=headers
+                )
+
+            if resp.status_code is 200:
+
+                try:
+
+                    resp_len = len(json.loads(
+                        resp.text).get("hits").get("hits")
+                        )
+
+                    if resp_len == 1:
+
+                        hits = json.loads(resp.text).get("hits").get("hits")
+                        virksomhed = hits[0].get("_source").get("Vrvirksomhed")
+                        cvr_no = virksomhed.get("cvrNummer", "")
+                        virk_meta = virksomhed.get("virksomhedMetadata")
+                        r_navn = virk_meta.get("nyesteNavn").get("navn", "")
+                        r_adresse = virk_meta.get("nyesteBeliggenhedsadresse")
+                        r_vejnavn = r_adresse.get("vejnavn", "")
+                        r_husnr = r_adresse.get("husnummerFra", "")
+                        r_postnr = r_adresse.get("postnummer", "")
+
+                        return {
+                            "cvr_no": cvr_no,
+                            "navn": r_navn,
+                            "vejnavn": r_vejnavn,
+                            "husnr": r_husnr,
+                            "postnr": r_postnr
+                        }
+
+                    else:
+
+                        # TODO: log(input, err) - Remove return statement
+
+                        return "No hit for -->{0}".format(navn)
+
+                except AttributeError as ae:
+
+                    # TODO: log(input, err) - Remove return statement
+
+                    return "AttributeError --> {0}".format(ae)
+
+            # if resp.status_code ...
+            else:
+
+                # TODO: log(input, err) - Remove return statement
+
+                return "HTTP Error --> {0}\nHTTP Body --> {1}".format(
+                    resp.status_code,
+                    resp.text
+                    )
+
+        # if org_name ....
+        else:
+
+            # TODO: log(input, err) - Remove return statement
+
+            return "ERROR: Company name and/or address info" \
+                    " missing in input dictionary."
+
+    # if virk_usr and ...
+    else:
+
+        # TODO: log(input, err) - Remove return statement
+
+        return "ERROR: Url and/or user credentials" \
+                " are missing in input dictionary."
+
 def get_cvr_no(params_dict):
     """Explanation pending
     """
@@ -24,6 +133,7 @@ def get_cvr_no(params_dict):
     # No login, no search.
     if virk_usr and virk_pwd and virk_url:
 
+        cvr = params_dict.get("cvr", None)
         org_name = params_dict.get("org_name", None)
         street_name = params_dict.get("street_name", None)
         house_no_from = params_dict.get("house_no_from", None)
@@ -56,8 +166,12 @@ def get_cvr_no(params_dict):
             usr = virk_usr
             pwd = virk_pwd
             headers = {"Content-type": "application/json; charset=UTF-8"}
-            payload = json.loads(populated_template)
-
+            
+            # json.decoder.JSONDecodeError does NOT LIKE the linebreaks in 
+            # the ElasticSearch query in the template. 
+            # Therefore we remove them before deserializing. 
+            payload = json.loads(populated_template.replace('\n',''))
+            
             resp = requests.post(
                 url,
                 auth=(usr, pwd),
