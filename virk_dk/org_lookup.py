05f17108f8235a92a46b6838ecbb315e818b1991
syre <thesyre@pm.me>
2021-06-16 15:12:18 +0200

    use FileSystemLoader

--- virk_dk/org_lookup.py
+++ virk_dk/org_lookup.py
@@ -11,10 +11,12 @@ import os
 import json
 import requests
 
-from jinja2 import Environment, PackageLoader, select_autoescape
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 env = Environment(
-    loader=PackageLoader("virk_dk"),
+    loader=FileSystemLoader(
+        os.path.join(os.path.dirname(__file__), 'templates')
+    ),
     autoescape=select_autoescape()
 )
 
